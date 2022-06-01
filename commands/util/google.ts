81fa6b8a782af1fc06b9d50cf3096ee655d59c88
llamaking136 <67032575+llamaking136@users.noreply.github.com>
2022-06-01 17:31:23 -0600

    no idea what's wrong with indentations

--- commands/util/google.ts
+++ commands/util/google.ts
@@ -3,73 +3,73 @@ import { makeEmbed } from '../../lib/embed';
 const GOOGLE_URL: string = "https://www.google.com/search?q=";
 
 const google = {
-	id: 'google', // Unique command identifier
-	name: ['google', 'lmgtfy'], // Command names
-	description: 'Google it', // Command description
-	hidden: false,
-	category: "Utility",
-	run: async (client, message, args) => {
-		let googleEmbed;
+    id: 'google', // Unique command identifier
+    name: ['google', 'lmgtfy'], // Command names
+    description: 'Google it', // Command description
+    hidden: false,
+    category: "Utility",
+    run: async (client, message, args) => {
+        let googleEmbed;
 
-		if(message.reference !== null && message.reference.messageId !== null)
-		{
-			// Search by replied message
-			const messageReference = await message.channel.messages.fetch(message.reference.messageId);
+        if(message.reference !== null && message.reference.messageId !== null)
+        {
+            // Search by replied message
+            const messageReference = await message.channel.messages.fetch(message.reference.messageId);
 
-			if(messageReference.content.length > 0)
-			{
-				let query: string = encodeURIComponent(messageReference.content).replace(/%20/g, '+');
-				query = query.replace('(', '%40');
-				query = query.replace(')', '%41');
+            if(messageReference.content.length > 0)
+            {
+                let query: string = encodeURIComponent(messageReference.content).replace(/%20/g, '+');
+                query = query.replace('(', '%40');
+                query = query.replace(')', '%41');
 
-				// Build URL
-				const url: string = GOOGLE_URL + query;
+                // Build URL
+                const url: string = GOOGLE_URL + query;
 
-				// Search by replied message
-				googleEmbed = makeEmbed({
-					title: 'Google it',
-					description: `[Here you go!](${url})`,
-				});
+                // Search by replied message
+                googleEmbed = makeEmbed({
+                    title: 'Google it',
+                    description: `[Here you go!](${url})`,
+                });
 
-				await messageReference.reply({ embeds: [googleEmbed] });
+                await messageReference.reply({ embeds: [googleEmbed] });
 
-				// Bail out
-				return;
-			}
-			else
-			{
-				googleEmbed = makeEmbed({
-					title: 'Google it',
-					description: 'No content to search for!',
-				});
-			}
-		}
-		else if(args.length > 0)
-		{
-			// Search by query
-			let query: string = encodeURIComponent(args.join(' ')).replace(/%20/g, '+');
-			query = query.replace('(', '%40');
-			query = query.replace(')', '%41');
+                // Bail out
+                return;
+            }
+            else
+            {
+                googleEmbed = makeEmbed({
+                    title: 'Google it',
+                    description: 'No content to search for!',
+                });
+            }
+        }
+        else if(args.length > 0)
+        {
+            // Search by query
+            let query: string = encodeURIComponent(args.join(' ')).replace(/%20/g, '+');
+            query = query.replace('(', '%40');
+            query = query.replace(')', '%41');
 
-			// Build URL
-			const url: string = GOOGLE_URL + query;
+            // Build URL
+            const url: string = GOOGLE_URL + query;
 
-			// Build embed
-			googleEmbed = makeEmbed({
-				title: 'Google it',
-				description: `[Here you go!](${url})`,
-			});
-		}
-		else
-		{
-			googleEmbed = makeEmbed({
-				title: 'Google it',
-				description: 'Please specify a search query.',
-			});
-		}
+            // Build embed
+            googleEmbed = makeEmbed({
+                title: 'Google it',
+                description: `[Here you go!](${url})`,
+            });
+        }
+        else
+        {
+            googleEmbed = makeEmbed({
+                title: 'Google it',
+                description: 'Please specify a search query.',
+            });
+        }
 
-		await message.reply({ embeds: [googleEmbed] });
-	}
+        await message.reply({ embeds: [googleEmbed] });
+    }
 };
 
 export const command = google;
